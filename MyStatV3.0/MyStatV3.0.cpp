c77b5e802c6b7079e5d2f005ae9680a91fd6558a
Martin Saprykin <sapr_dp83@student.itstep.net>
2022-10-02 17:39:59 +0200

    Add project files.

---
+++ MyStatV3.0/MyStatV3.0.cpp
@@ -0,0 +1,49 @@
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+#include "Tests.h"
+#include <iostream>
+#include <string>
+#include <windows.h>
+ 
+using namespace std;
+int main()
+{
+	Student student;
+	Teacher teacher;
+	Menu menu({"Student","Teacher","Exit"});
+	while (true)
+	{
+		menu.drawOptions();
+		switch (keymenu())
+		{
+		case VK_UP:
+		{
+			menu.up();
+			menu.drawOptions();
+			break;
+		}
+		case VK_DOWN:
+		{
+			menu.down();
+			menu.drawOptions();
+			break;
+		}
+		case ENTER:
+		{
+			system("cls");
+			switch (menu.getSelectedOption())
+			{
+			case STUDENT:
+				student.run();
+				break;
+			case TEACHER:
+				teacher.run();
+				break;
+			case MainMenuOptions::EXIT:
+				return 0;
+			}
+		}
+		}
+	}
+}
